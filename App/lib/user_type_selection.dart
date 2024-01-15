65fc1e4d727e558e1fca8328f1a703d31c6382a4
ilefchaabane <95141802+ilefchaabane@users.noreply.github.com>
2024-01-15 12:01:30 +0100

    add login page

---
+++ App/lib/user_type_selection.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'judge_login.dart' as judge;
+import 'president_login.dart' as president;
+
+class UserTypeSelectionPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select User Type'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blue, Colors.indigo],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _buildLoginButton(context, 'Judge Login', judge.JudgeLoginPage()),
+              SizedBox(height: 20),
+              _buildLoginButton(
+                  context, 'President Login', president.PresidentLoginPage()),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoginButton(BuildContext context, String text, Widget page) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => page),
+        );
+      },
+      child: Text(text),
+    );
+  }
+}
